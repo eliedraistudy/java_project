db0796c4bf843abe475b92dccec7ca0eb54c5ead
Elie Drai <elie.drai.study@gmail.com>
2018-03-15 16:50:48 +0200

    Create classes of geometry package

--- src/geometries/Geometry.java
+++ src/geometries/Geometry.java
@@ -1,6 +1,7 @@
 package geometries;
 
 
+import javafx.scene.paint.Color;
 import primitives.Material;
 
 public class Geometry
@@ -9,6 +10,20 @@ public class Geometry
     private Material _material;
     private double _nShininess;
     private Color _emission;
+
+
+
+    //******************* CONSTRUCTORS *******************//
+
+    /**
+     * Default consructor
+     */
+    public Geometry()
+    {
+        _material = new Material();
+        _nShininess = 1;
+        _emission = new Color(0,0,0);
+    }
 
 
 }
--- src/primitives/Material.java
+++ src/primitives/Material.java
@@ -38,14 +38,64 @@ public class Material
 
 
     // ***************** Getters/Setters ********************** //
+
+    /**
+     * Getter for diffusion attenuation coefficient
+     * @return value of _kd
+     */
     public double getKd() { return _Kd; }
+
+    /**
+     * Getter for specular attenuation coefficient
+     * @return value of _ks
+     */
     public double getKs() { return _Ks; }
+
+    /**
+     * Getter for reflection coefficient
+     * @return value of _kr
+     */
     public double getKr() { return _Kr; }
+
+    /**
+     * Getter for refraction coefficient
+     * @return value of _kt
+     */
     public double getKt() { return _Kt; }
+
+    /**
+     * Getter for refraction index
+     * @return value of _n
+     */
     public double getN() { return _n; }
+
+    /**
+     * Setter for diffusion attenuation coefficient
+     * @param Kd value to set
+     */
     public void setKd(double Kd) { _Kd = Kd; }
+
+    /**
+     * Setter for specular attenuation coefficient
+     * @param Ks value to set
+     */
     public void setKs(double Ks) { _Ks = Ks; }
+
+    /**
+     * Setter for reflection coefficient
+     * @param Kr value to set
+     */
     public void setKr(double Kr) { _Kr = Kr; }
+
+    /**
+     * Setter for refraction coefficient
+     * @param Kt value to set
+     */
     public void setKt(double Kt) { _Kt = Kt; }
+
+    /**
+     * Setter for refraction index
+     * @param n value to set
+     */
     public void setN (double n) { _n = n; }
 }
