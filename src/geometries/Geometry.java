4bccfaf5f0836e7e94e428321ec20e5ee7bf93dd
Elie Drai <elie.drai.study@gmail.com>
2018-03-18 20:53:48 +0200

    continue

--- src/geometries/Geometry.java
+++ src/geometries/Geometry.java
@@ -7,23 +7,9 @@ import primitives.Material;
 public class Geometry
 {
     //******************* FIELDS *******************//
-    private Material _material;
-    private double _nShininess;
-    private Color _emission;
-
-
-
-    //******************* CONSTRUCTORS *******************//
-
-    /**
-     * Default consructor
-     */
-    public Geometry()
-    {
-        _material = new Material();
-        _nShininess = 1;
-        _emission = new Color(0,0,1,1.0);
-    }
+    private Material _material = new Material();
+    private double _nShininess = 1;
+    private Color _emission = new Color(0,0,1,1.0);
 
 
 }
