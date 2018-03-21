013c900bca82068adde709414419a69d3e099924
shlomfix <shlomibirs@gmail.com>
2018-03-22 01:13:49 +0200

    addition tests for Point3D

--- tests/primitives/Point3DTest.java
+++ tests/primitives/Point3DTest.java
@@ -1,5 +1,6 @@
 package primitives;
 
+import javafx.scene.effect.Light;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -37,31 +38,43 @@ public class Point3DTest {
         Point3D p1 = new Point3D();
         Point3D p2 = new Point3D(-4,3,-7);
         p1.setPoint(p2);
+        assertEquals(p1.compareTo(p2),0,0);
+    }
 
+    /**
+     * tests if two object references do not point to the same object
+     */
+    @Test
+    public void setPoint2()
+    {
+        Point3D p1 = new Point3D();
+        Point3D p2 = new Point3D(-4,3,-7);
+        p1.setPoint(p2);
 
-            if (p1 == null && p1 == null)
-            {
-                return;
-            }
-            if (p1 != null && p1.equals(p2))
-            {
-                return;
-            }
-
-            //failNotEquals("setPoint test is fail", p1, p2); // eli, we need to check how to compare between objects, it's don't work for me/
-
+        assertNotSame(p1,p2);
     }
 
     @Test
-    public void compareTo() {
+    public void compareTo()
+    {
+        Point3D p1 = new Point3D(3,-4,5);
+        Point3D p2 = new Point3D(3,-4,5);
+        assertEquals(p1.compareTo(p2), 0,0);
     }
 
     @Test
-    public void testToString() {
+    public void testToString()
+    {
+        Point3D p = new Point3D(3.22456,5.01234,6.9876);
+        assertEquals(p.toString(), "(3.22, 5.01, 6.99)");
     }
 
     @Test
-    public void add() {
+    public void add()
+    {
+        Point3D p1 = new Point3D(-3,5,1);
+        Point3D p2 = new Point3D(7,-8,0);
+     //   assertEquals(p1.add(p2),n);
     }
 
     @Test
@@ -69,30 +82,53 @@ public class Point3DTest {
     }
 
     @Test
-    public void distance() {
+    public void distance()
+    {
+        //Point3D p = new Point3D(-2,2,-1);
+
     }
 
     @Test
-    public void getX() {
+    public void getX()
+    {
+        Point3D p = new Point3D(-2,2,-1);
+        assertEquals(-2,p.getX().getCoordinate(),0);
     }
 
     @Test
-    public void getXValue() {
+    public void getXValue()
+    {
+        Point3D p = new Point3D(-32.667,2,-1);
+        assertEquals(-32.667,p.getXValue(),0);
     }
 
     @Test
-    public void getY() {
+    public void getY()
+    {
+        Point3D p = new Point3D(-32.667,2.234,-1);
+        assertEquals(2.234,p.getY().getCoordinate(),0);
     }
 
     @Test
-    public void getYValue() {
+    public void getYValue()
+    {
+        Point3D p = new Point3D(-32.667,2.234,-1);
+        assertEquals(2.234,p.getYValue(),0);
     }
 
     @Test
-    public void setX() {
+    public void setX()
+    {
+        Point3D p = new Point3D(-32.667,2.234,-1);
+        p.setX(new Coordinate(10.27));
+        assertEquals(10.27,p.getXValue(),0);
     }
 
     @Test
-    public void setY() {
+    public void setY()
+    {
+        Point3D p = new Point3D(-32.667,2.234,-1);
+        p.setY(new Coordinate(10.27));
+        assertEquals(10.27,p.getYValue(),0);
     }
 }
---
+++ tests/primitives/VectorTest.java
@@ -0,0 +1,87 @@
+package primitives;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class VectorTest {
+
+    @Test
+    public void getHead()
+    {
+        Vector v = new Vector(1,2,3);
+        assertEquals(v.getHead().compareTo(new Point3D(1,2,3)),0,0);
+    }
+
+    @Test
+    public void getX()
+    {
+        
+    }
+
+    @Test
+    public void getY() {
+    }
+
+    @Test
+    public void getZ() {
+    }
+
+    @Test
+    public void setHead() {
+    }
+
+    @Test
+    public void setHead1() {
+    }
+
+    @Test
+    public void compareTo() {
+    }
+
+    @Test
+    public void toStringTest() {
+    }
+
+    @Test
+    public void add() {
+    }
+
+    @Test
+    public void subtract() {
+    }
+
+    @Test
+    public void scale() {
+    }
+
+    @Test
+    public void crossProduct() {
+    }
+
+    @Test
+    public void length() {
+    }
+
+    @Test
+    public void normalize() {
+    }
+
+    @Test
+    public void normalVector() {
+    }
+
+    @Test
+    public void dotProduct() {
+    }
+
+    @Test
+    public void isNull() {
+    }
+
+    @Test
+    public void nullTest()
+
+    {
+    }
+}
