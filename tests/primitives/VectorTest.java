82d0a3a50bf8b2634cdeae603fe7ba61bba2f48a
shlomfix <shlomibirs@gmail.com>
2018-03-22 15:02:25 +0200

    addition tests for Vector

--- tests/primitives/VectorTest.java
+++ tests/primitives/VectorTest.java
@@ -16,27 +16,48 @@ public class VectorTest {
     @Test
     public void getX()
     {
-        //
+        Vector v = new Vector(-10.345,2,3);
+        assertEquals(v.getX(), -10.345, 0 );
+
     }
 
     @Test
-    public void getY() {
+    public void getY()
+    {
+        Vector v = new Vector(-10.345,2.4567778,3);
+        assertEquals(v.getY(), 2.4567778, 0 );
     }
 
     @Test
-    public void getZ() {
+    public void getZ()
+    {
+        Vector v = new Vector(-10.345,2.4567778,0.345);
+        assertEquals(v.getZ(), 0.345, 0 );
     }
 
     @Test
-    public void setHead() {
+    public void setHead()
+    {
+        Point3D p = new Point3D(-10.345,2.4567778,0.345);
+        Vector v = new Vector();
+        v.setHead(p);
+
+        assertEquals(v.getHead().compareTo(p),0,0);
+
     }
 
     @Test
-    public void setHead1() {
+    public void setHead1()
+    {
+        Vector v = new Vector();
+        v.setHead(34.67,-32.5447,0.112);
+        assertEquals(v.getHead().compareTo(new Point3D(34.67,-32.5447,0.112)),0,0);
     }
 
     @Test
-    public void compareTo() {
+    public void compareTo()
+    {
+
     }
 
     @Test
